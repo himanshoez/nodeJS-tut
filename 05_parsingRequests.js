2f67db5c7b80ca3027bdadeb3492fa356cb19575
Himanshoeh <himanshutaneja1803@gmail.com>
2025-06-17 18:56:44 +0530

    Parsing requests in ExpressJS

---
+++ 05_parsingRequests.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+
+const app = express();
+
+app.use(bodyParser.urlencoded({extended: false}));
+
+app.use('/add-product', (req, res, next)=>{
+
+    res.send('<form action ="/product" method = "POST"><input type= "text" name = "Product" placeholder = "Enter the product"><button>Add Product</button></form>');    
+})
+
+app.use('/product', (req, res, next)=>{
+    console.log(req.body);
+    res.redirect('/');
+})
+
+app.use('/', (req, res, next)=>{
+    res.send('<h1>Hello From Express</h1>');
+})
+
+app.listen(3000)
