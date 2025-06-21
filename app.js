6e89685c65c2695e1189a29ef3679e4928128f5e
Himanshoeh <himanshutaneja1803@gmail.com>
2025-06-21 14:51:26 +0530

    removed HandleBar, Pug and their layout files

--- app.js
+++ app.js
@@ -5,24 +5,23 @@ const path = require('path')
 const adminData = require('./routes/admin')
 const shopRoutes = require('./routes/shop')
 
-const expressHbs = require('express-handlebars');
 
 const rootDir = require('./utilities/path')
 
 const app = express();
 
-app.engine('handlebars', expressHbs.engine());
-app.set('view engine', 'handlebars');
-// app.set('view engine', 'pug'); // To tell express what template engine we are using
+// app.engine('handlebars', expressHbs.engine());
+// app.set('view engine', 'handlebars');
+app.set('view engine', 'ejs'); // To tell express what template engine we are using
 app.set('views', 'views'); // To tell express where to use the template engine
 
 app.use(bodyParser.urlencoded({extended: false}));
+app.use(express.static(path.join(__dirname, 'public'))) //statically serves the css files
 
 // filtering routes
 
 app.use('/admin', adminData.routes);
 app.use(shopRoutes);
-app.use(express.static(path.join(__dirname, 'public'))) //statically serves the css files
 
 app.use((req,res,next)=>{
     res.status(404).render('404', {pageTitle: 'Page Not Found'});
--- routes/admin.js
+++ routes/admin.js
@@ -9,7 +9,14 @@ const products = [];
 
 router.get('/add-product', (req, res, next)=>{
 
-    res.sendFile(path.join(rootDir, 'views', 'add-product.html'));    
+    // res.sendFile(path.join(rootDir, 'views', 'add-product.html'));
+    res.render('add-product', {
+        pageTitle: "Add Product",
+        path : '/admin/add-product',
+        formsCSS: true,
+        productCSS:  true,
+        activeAddProduct: true
+    })    
 })
 
 //  middleware will only listen to POST requests 
